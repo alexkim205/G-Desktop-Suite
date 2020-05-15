ea209d7aaf96258cc151f12e02f9a504f5f78659
Alex Kim <alexgkim205@gmail.com>
2020-05-15 13:57:39 -0400

    fix cut copy paste by reenabling edit menu

--- src/javascripts/menu.js
+++ src/javascripts/menu.js
@@ -33,19 +33,19 @@ var template = [{
       // { label: "Close Window", accelerator: "CmdOrCtrl+W", role: 'close' }
     ]
   },
-  // {
-  //   label: "Edit",
-  //   role: 'editMenu'
-  //   // submenu: [
-  //   //   { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-  //   //   { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-  //   //   { type: "separator" },
-  //   //   { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-  //   //   { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-  //   //   { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-  //   //   { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-  //   // ]
-  // },
+  {
+    label: "Edit",
+    role: 'editMenu'
+    // submenu: [
+    //   { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+    //   { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+    //   { type: "separator" },
+    //   { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+    //   { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+    //   { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+    //   { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    // ]
+  },
   {
     label: "View",
     submenu: [
