0cb4fca5accd4f42cabb4e3926d6267fdac1c9ef
mding5692 <m.ding5692@gmail.com>
2020-05-16 18:35:14 -0400

    Removes unsupported warning on MacOS

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const userAgent = "Chrome";
+const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36";
 const signInURL =
   "https://accounts.google.com/signin/v2/identifier?service=wise&passive=true&continue=http%3A%2F%2Fdrive.google.com%2F%3Futm_source%3Den&utm_medium=button&utm_campaign=web&utm_content=gotodrive&usp=gtd&ltmpl=drive&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 
