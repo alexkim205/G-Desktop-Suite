d6346536f210fdde4dea69617bce4803ee6f43c8
mding5692 <m.ding5692@gmail.com>
2020-05-19 22:54:54 -0400

    Adds os platform constants to config

--- src/config.js
+++ src/config.js
@@ -2,4 +2,17 @@ const userAgent = "Mozilla/5.0 (Macintosh; ; ) AppleWebKit/537.36 (KHTML, like G
 const signInURL =
   "https://accounts.google.com/signin/v2/identifier?service=wise&passive=true&continue=http%3A%2F%2Fdrive.google.com%2F%3Futm_source%3Den&utm_medium=button&utm_campaign=web&utm_content=gotodrive&usp=gtd&ltmpl=drive&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
 
-module.exports = { userAgent, signInURL };
+const osPlatform = process.platform;
+const CONSTANTS = {
+  OS_PLATFORMS: {
+    MAC_OS: 'darwin',
+    WINDOWS: 'win32',
+    LINUX: 'linux',
+    AIX: 'aix',
+    FREEBSD: 'freebsd',
+    OPENBSD: 'openbsd',
+    SUN_OS: 'sunos',
+  }
+}
+
+module.exports = { userAgent, signInURL, osPlatform, CONSTANTS };
